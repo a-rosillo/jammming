6c26d9c08b7ba2b6fcdcaa53cfec05db288a3b5a
a-rosillo <128966633+a-rosillo@users.noreply.github.com>
2023-05-09 07:12:34 +0100

    created components. added form to search component.

---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function SearchBar () {
+    return(
+        <form>
+            <h1>I'm working :)</h1>
+            <input type='search' name='search' />
+            <input type='submit' value='Search' />
+        </form>
+    )
+}
+
+export default SearchBar;
