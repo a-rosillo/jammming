0847cafa327c024c9cacd55a15124d51a5fdeac4
a-rosillo <128966633+a-rosillo@users.noreply.github.com>
2023-05-25 07:04:42 +0100

    obtaind url hash fragment paramaters as js object.

--- src/utilities/Spotify.js
+++ src/utilities/Spotify.js
@@ -14,14 +14,59 @@ function getAccessToken () {
     url += '&scope=playlist-modify-public'
     window.location = url
     const returnedURL = window.location.href
-    const hashFragmentIndex = returnedURL.indexOf('#')
-    const hashFragment = returnedURL.slice(hashFragmentIndex + 1)
-    alert(hashFragment)
-    const accessTokenIndex = hashFragment.indexOf('&')
-    alert(accessTokenIndex)
-    accessToken = hashFragment.slice(0, accessTokenIndex)
-    alert(accessToken)
+    getHashParams(returnedURL)
     }
+
+function getHashParams (url){
+    const hashFragmentIndex = url.indexOf('#')
+    const hashFragment = url.slice(hashFragmentIndex)
+    let currentAmpersandIndex = hashFragment.indexOf('&')
+    const ampersandIndicies =[]
+    while (currentAmpersandIndex !== -1){
+        ampersandIndicies.push(currentAmpersandIndex)
+        currentAmpersandIndex = hashFragment.indexOf('&', currentAmpersandIndex + 1)
+    }
+
+    const keyValuePairs = getKeyValuePairs(hashFragment, ampersandIndicies)
+    const hashParams = createObjectFromArrayOfPairs(keyValuePairs)
+
+    console.log(hashParams)
+    return hashParams
+}
+
+function getKeyValuePairs (string, indicies){
+    const pairs = []
+    let start = null
+    let end = null
+    for (let index = 0; index < indicies.length + 1; index++) {
+        if(index === 0){
+            start = 0
+        } else {
+            start = indicies[index - 1]
+        }
+        if(index === indicies.length){
+            end = undefined
+        } else {
+            end = indicies[index]
+        }
+        pairs.push(string.slice(start + 1, end)) //the +1 is to remove the # or & from the key
+    }
+    return pairs
+}
+
+function createObjectFromArrayOfPairs (arrayOfPairs) {
+    let equalsIndex = null
+    let key = ''
+    let value = ''
+    const params = {}
+    arrayOfPairs.forEach((keyValuePair) => {
+        equalsIndex = keyValuePair.indexOf('=')
+        key = keyValuePair.slice(0,equalsIndex)
+        value = keyValuePair.slice(equalsIndex + 1)
+        params[key] = value
+    })
+    return params
+}
 
 async function searchSpotify(text) {
     // const endpoint = 'https://api.spotify.com/v1/search'
@@ -48,5 +93,6 @@ async function searchSpotify(text) {
     // return formattedTracks
 }
 
+getHashParams('http://localhost:3000/#access_token=BQDS_L2yLz-f_7C6fcsOs1yQuyu7n665L69mdp3kNNBUnw6e4XPZO5kRck__dkVR8QCb_ZTrnoIBVln-TYfHCRjvMYaKfQ4TVBSWxb33vOx9OGyyYdy7oOT_gFjXzCarhzeWxcuI45jjfBI5Hbpqr0MAVpRyhUs2GjFXnFFoDiDEN-2Vb44jBNCRzTk9hhTfrwL71Fgvbhgh&token_type=Bearer&expires_in=3600')
 
-export { getAccessToken, searchSpotify }
+//export { getAccessToken, searchSpotify }
